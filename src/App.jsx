ef67c909977bb42f17da5aa5e6bf1696f45dbedb
Amos Barbato <amosbarbato@gmail.com>
2024-10-02 12:12:51 -0300

    chore: created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
+import Header from "./components/header";
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <Header />;
 }
